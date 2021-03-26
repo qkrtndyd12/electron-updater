92903bb947ce2d4ead9c1b66238f3b617c7cc962
박수용 <dfort123@nate.com>
2021-03-26 17:54:37 +0900

    version 1.0.1

--- src/index.js
+++ src/index.js
@@ -44,3 +44,65 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
+const { autoUpdater } = require("electron-updater");
+
+let updateWin;
+
+function sendStatusToWindow(text) {
+  updateWin.webContents.send("message", text);
+}
+
+function createDefaultUpdaetWindow() {
+  updateWin = new BrowserWindow({
+    backgroundColor: "#eeeeee",
+    webPreferences: { nodeIntegration: true },
+  });
+
+  updateWin.on("closed", () => {
+    updateWin = null;
+  });
+  updateWin.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+  return updateWin;
+}
+
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
+});
+autoUpdater.on("update-available", (info) => {
+  sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", (info) => {
+  sendStatusToWindow("Update not available.");
+});
+autoUpdater.on("error", (err) => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+autoUpdater.on("download-progress", (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on("update-downloaded", (info) => {
+  sendStatusToWindow("Update downloaded");
+
+  const option = {
+    type: "question",
+    buttons: ["업데이트", "취소"],
+    defaultId: 0,
+    title: "electron-updater",
+    message: "업데이트가 있습니다. 프로그램을 업데이트 하시겠습니까?",
+  };
+  let btnIndex = dialog.showMessageBoxSync(updateWin, option);
+
+  if(btnIndex === 0) {
+    autoUpdater.quitAndInstall();
+  }
+});
+
+app.on("ready", async () => {
+  createDefaultUpdaetWindow();
+  autoUpdater.checkForUpdates();
+});
